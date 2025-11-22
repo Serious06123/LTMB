e4052fb9b341a5db73d26d088ff8b2559f46cbe9
JohanCorey <nguyenhoangchuong5541@gmail.com>
2025-11-22 23:07:05 +0700

    TaskCode01: Thêm icon app + giới thiệu khi vào app

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,35 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useSelector } from 'react-redux'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import HomeScreen from '../screens/Home/HomeScreen';
 import LoginScreen from '../screens/Auth/LoginScreen';
 import CartScreen from '../screens/Cart/CartScreen';
-
+import IntroScreen from '../screens/Auth/Intro';
 const Stack = createNativeStackNavigator();
 
 export default function Navigators() {
-  const token = useSelector((state : any) => state.generalState.token);
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {token ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Cart" component={CartScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />         
-            {/* Bạn có thể thêm các màn hình chính khác (Profile, Settings, v.v.)
-              vào bên trong <></> này.
-            */}
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </>
-        )}
+      <Stack.Navigator initialRouteName='Intro' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Intro" component={IntroScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
